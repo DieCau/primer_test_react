efb28a12ab2348e378c1a97079b0903cb228b93b
DiCa! <diecau20@gmail.com>
2021-11-09 20:31:57 -0300

    Probando testing en React

---
+++ src/PrimeraApp.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+
+
+
+  return (
+    <>
+      <h1> { saludo } </h1>
+      <p> { subtitulo } </p>
+    </>
+  );
+};
+
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+  subtitulo: 'Soy un subtitulo por defecto'
+}
+
+export default PrimeraApp;
